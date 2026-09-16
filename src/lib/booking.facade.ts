9a95bd28ff17d2bc811388efd30b1d0ee2b0a374
deumgamerguia-svg <deumgamerguia@gmail.com>
2026-09-16 15:16:34 -0300

    feat: conectar disponibilidade publica ao novo painel 2

---
+++ src/lib/booking.facade.ts
@@ -0,0 +1,12 @@
+export {
+  getPublicAvailability as getAvailability,
+  getPublicOpenDays as getOpenDays,
+} from "./public-availability.functions";
+
+export {
+  reserveBooking,
+  generateDepositPix,
+  cancelDepositBooking,
+  getMyBookings,
+  getDepositStatus,
+} from "./booking.functions";
