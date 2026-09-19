c7565813c17045297fb2e5c1067ff90b65da7c02
deumgamerguia-svg <deumgamerguia@gmail.com>
2026-09-19 19:33:20 -0300

    perf: defer toasts in Professionals route

--- src/routes/_authenticated/painel.profissionais.tsx
+++ src/routes/_authenticated/painel.profissionais.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useServerFn } from "@tanstack/react-start";
-import { toast } from "sonner";
+import { toast } from "@/lib/toast";
 import {
   BadgeCheck,
   BriefcaseBusiness,
